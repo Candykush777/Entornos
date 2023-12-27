c5811a95f86d29416409bdbbb61da4df430c1a02
Eduardo Afán <eduardoafan.eae@gmail.com>
2023-12-27 23:05:27 +0100

    update

---
+++ src/Branch_Projects/Base.java
@@ -0,0 +1,24 @@
+package Branch_Projects;
+
+import java.util.Scanner;
+
+public class Base {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int suma = 0;
+        int resta = 0;
+        int multiplicacion = 0;
+        int division = 0;
+        System.out.println("Ingrese valor A : ");
+        int a = scanner.nextInt();
+        System.out.println("Ingrese Valor B : ");
+        int b = scanner.nextInt();
+        System.out.println("\n Resultado Suma : " + suma);
+        System.out.println("\n Resultado de la Resta : " + resta);
+        System.out.println("\n Resultado de la División : " + division);
+        System.out.println("\n Resultado de la Multiplicación : " + multiplicacion);
+        System.out.println();
+
+    }
+
+}
