fd6ea23f46655debd381fb417938f6a1492537af
Mario <villamario7@gmail.com>
2023-12-29 15:16:34 +0100

    update

--- src/Branch_Projects/metodos.java
+++ src/Branch_Projects/metodos.java
@@ -1,4 +1,23 @@
 package Branch_Projects;
 
 public class metodos {
+
+    public class sumar{
+
+         sumar =  a + b;
+    }
+
+    public class restar{
+         sumar =  a - b;
+    }
+
+    public class division{
+        sumar =  a / b;
+    }
+
+    public class multiplicacion{
+        sumar =  a * b;
+    }
+
+
 }
