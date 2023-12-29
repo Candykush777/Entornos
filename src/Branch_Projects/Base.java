ee4b73fd374bde5e46bea80242c25779856f1bb7
Eduardo Afán <eduardoafan.eae@gmail.com>
2023-12-29 22:17:59 +0100

    update

--- src/Branch_Projects/Base.java
+++ src/Branch_Projects/Base.java
@@ -5,42 +5,41 @@ import java.util.Scanner;
 public class Base {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int suma = 0;
+        int resta = 0;
+        int multiplicacion = 0;
+        int division = 0;
         System.out.println("Ingrese valor A : ");
         int a = scanner.nextInt();
         System.out.println("Ingrese Valor B : ");
         int b = scanner.nextInt();
         System.out.println("Ingresa operador: ");
-        int operator = scanner.nextInt();
+        char operator;
+        operator = scanner.next().charAt(0);
 
 
-        switch (operator) {
-            case 1:
-                int suma = a + b;
-                System.out.println("\n Resultado Suma : " + a + " + " + b + " = " + suma);
+        switch (operator){
+            case '+':
+                System.out.println("\n Resultado Suma : " + a + "+" + b + "=" + suma);
                 break;
 
-            case 2:
-                int resta = a - b;
-                System.out.println("\n Resultado de la Resta : " + a + " - " + b + " = " + resta);
+            case '-':
+                System.out.println("\n Resultado de la Resta : " + a + "-" + b + "=" + resta);
                 break;
 
-            case 3:
-                if (b != 0) {
-                    double division = (double) a / b;
-                    System.out.println("\n Resultado de la División : " + a + " / " + b + " = " + division);
-                } else {
-                    System.out.println("\n Resultado de la División : " + a + " / " + b + " = 0");
-                }
+            case '/':
+                System.out.println("\n Resultado de la División : " + a + "/" + b + "=" + division);
                 break;
 
-            case 4:
-                int multiplicacion = a * b;
-                System.out.println("\n Resultado de la Multiplicación : " + a + " * " + b + " = " + multiplicacion);
+            case '*':
+                System.out.println("\n Resultado de la Multiplicación : " + a + "*" + b + "=" + multiplicacion);
                 break;
 
             default:
-                System.out.println("Operador no válido");
+                System.out.println("No valido");
+
         }
     }
+
 }
 
